41aff2dc7c89958f1f0f7fefaf39cc07f9c3116f
DleiaDev <markowebdeveloper@gmail.com>
2024-03-19 19:04:51 +0100

    Begin writing LazyFile event

--- nvim/lua/util/plugin.lua
+++ nvim/lua/util/plugin.lua
@@ -6,7 +6,7 @@ end
 
 -- Setup "LazyFile" event that allows us to load plugins without blocking the UI
 function M.setup_lazy_file_event()
-  local Event = require "lazy.core.handler.event"
-
-  vim.print(Event)
+  --
 end
+
+return M
