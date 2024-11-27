d136adefc45e96a3de618468bdd7d27435cd56e1
DleiaDev <markowebdeveloper@gmail.com>
2024-11-27 12:19:08 +0100

    refactor: remove links.prop

--- install.sh
+++ install.sh
@@ -1,23 +1,8 @@
-#!/usr/bin/env zsh
+#!/usr/bin/zsh
 
 DOTFILES=$(pwd -P)
 
-set -e
-
-local overwrite_all=false
-local backup_all=false
-local skip_all=false
-
-find $DOTFILES -maxdepth 2 -name 'links.prop' | while read linkfile
-do
-  cat $linkfile | while read line
-  do
-    local src=$(eval echo $line | cut -d = -f 1)
-    local dst=$(eval echo $line | cut -d = -f 2)
-    local dst_dir=$(dirname $dst)
-
-    mkdir -p $dst_dir
-    ln -sf $src $dst
-  done
-done
-
+ln -sf $DOTFILES/kitty $HOME/.config/kitty
+ln -sf $DOTFILES/nvim $HOME/.config/nvim
+ln -sf $DOTFILES/wezterm $HOME/.config/wezterm
+ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
