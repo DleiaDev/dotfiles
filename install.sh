6113244b00215f95dd03e4b45f93b898f5ee66d4
marko-auvaria <m.ilic@auvaria.com>
2025-12-24 23:35:55 +0100

    wip: harlequin

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -sf $DOTFILES/kitty $HOME/.config/kitty
 ln -sf $DOTFILES/nvim $HOME/.config/nvim
 ln -sf $DOTFILES/wezterm $HOME/.config/wezterm
 ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES/harlequin/harlequin.toml $HOME/harlequin/harlequin.toml
