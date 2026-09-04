f0babae28f52796a3dd3dc3f06914e5de85da264
DleiaDev <markowebdeveloper@gmail.com>
2026-09-04 15:53:51 +0200

    feat: PySource command

--- nvim/lua/plugins/lang/python.lua
+++ nvim/lua/plugins/lang/python.lua
@@ -1,5 +1,15 @@
 local nls = require("null-ls")
 
+vim.api.nvim_create_user_command("PySource", function(opts)
+  code = "import importlib.util as u, sys; s = u.find_spec(sys.argv[1]); print(getattr(s, 'origin', ''))"
+  res = vim.trim(vim.fn.system({ "python3", "-c", code, opts.args }))
+  if res == "" or res:match("%.pyi$") then
+    vim.notify("No .py source for " .. opts.args)
+    return
+  end
+  vim.cmd.edit(res)
+end, { nargs = 1, desc = "Open real source of Python module" })
+
 return {
   "nvimtools/none-ls.nvim",
   opts = {
